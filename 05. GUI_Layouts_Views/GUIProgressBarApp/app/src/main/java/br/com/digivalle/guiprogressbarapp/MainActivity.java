fd5fdc386cb10a915632d9c08231528ca8b00b92
Rafael S Valle <rafaelsouzavalle@gmail.com>
2020-02-26 05:27:55 -0300

    Do a GUI App using ProgressBar
    Do a GUI App using ProgressBar View and FrameLayout

---
+++ 05. GUI_Layouts_Views/GUIProgressBarApp/app/src/main/java/br/com/digivalle/guiprogressbarapp/MainActivity.java
@@ -0,0 +1,46 @@
+package br.com.digivalle.guiprogressbarapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textview_message);
+        progressBar = findViewById(R.id.progress);
+
+        textView.setAlpha(0.3f);
+
+        loadContent();
+    }
+
+    private void loadContent() {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                SystemClock.sleep(5000);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.animate().setDuration(5000).alpha(1.0f);
+                        progressBar.animate().setDuration(5000).alpha(0.0f);
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
+            }
+        }).start();
+    }
+}
