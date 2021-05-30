ecb2311b6f98c0a6b1b69fb1a4d111379d08c269
Rafael <rafaelsouzavalle@gmail.com>
2021-05-30 19:02:20 -0300

    Make a RecyclerView App with custom adapter
    Make a RecyclerView App with custom adapter

---
+++ 07.GUI_Adapters_and_Dialogs/RecyclerViewApp/app/src/main/java/br/com/digivalle/recyclerviewapp/ItemAdapter.java
@@ -0,0 +1,59 @@
+package br.com.digivalle.recyclerviewapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemHolder> {
+
+
+    private List<Item> items = new ArrayList<>();
+    private Context context;
+
+    public ItemAdapter(Context context) {
+        this.context = context;
+
+        for(int i = 0; i < 100; i++) {
+            items.add(new Item(i + 1, "Item " + (i + 1)));
+        }
+    }
+
+    @NonNull
+    @Override
+    public ItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_2, parent, false);
+        ItemHolder holder = new ItemHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemHolder holder, int position) {
+        Item item = items.get(position);
+        holder.txtId.setText(String.valueOf(item.getId()));
+        holder.txtName.setText(item.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public static class ItemHolder extends RecyclerView.ViewHolder {
+        public TextView txtId;
+        public TextView txtName;
+
+        public ItemHolder(@NonNull View itemView) {
+            super(itemView);
+            txtId = itemView.findViewById(android.R.id.text1);
+            txtName = itemView.findViewById(android.R.id.text2);
+        }
+    }
+}
---
+++ 07.GUI_Adapters_and_Dialogs/RecyclerViewApp/app/src/main/java/br/com/digivalle/recyclerviewapp/MainActivity.java
@@ -0,0 +1,23 @@
+package br.com.digivalle.recyclerviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ItemAdapter adapter = new ItemAdapter(this);
+        recyclerView.setAdapter(adapter);
+    }
+}
